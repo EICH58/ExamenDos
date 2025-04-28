3e776da94e4e5c8d2f75de28ec9ff6a8525a3aaf
EICH58 <165859468+EICH58@users.noreply.github.com>
2025-04-28 06:49:10 -0700

    Update EmpleadosTest.java

--- src/test/java/com/itt/java/ExamenDOS/EmpleadosTest.java
+++ src/test/java/com/itt/java/ExamenDOS/EmpleadosTest.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.awt.GraphicsEnvironment;
 
 
 class EmpleadosTest {
